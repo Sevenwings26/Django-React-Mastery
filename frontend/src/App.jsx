2522abc8ab8fc15b7311c65f8ddc61682f9b2e73
Sevenwings26 <iarowosola@yahoo.com>
2024-08-06 06:32:54 +0100

    Navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Nav from "./components/Nav"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <h2>In progress</h2>
+        < Nav/>
+        < Routes>
+          {/* < Route path='/' element={<} */}
+        </Routes>
+      </Router>
+    </>
   )
 }
 
